19d6d65d08905f9bb6fce45f55b96f71082dd2c1
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-11 16:25:09 -0400

    Add types

---
+++ src/app/types/tweet.types.ts
@@ -0,0 +1,10 @@
+type urlImage = `https://i.pravatar.cc/150?u=a${string}`
+
+interface TweestType {
+  textMarkdown: string
+  username: string
+  nickname: string
+  urlImage: urlImage
+}
+
+export type { TweestType }
