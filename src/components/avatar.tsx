4597f93fe747d00a403f01fa067c3f78a38a4cb6
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-11 11:25:44 -0400

    Add className props

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -3,16 +3,17 @@ import Image from 'next/image'
 interface AvatarProps {
   src: string
   name: string
+  className?: string
 }
 
-function Avatar ({ src, name }: AvatarProps) {
+function Avatar ({ src, name, className }: AvatarProps) {
   return (
     <Image
       src={src}
       alt={name}
       width={48}
       height={48}
-      className='rounded-full'
+      className={`rounded-full ${className}`}
     />
   )
 }
