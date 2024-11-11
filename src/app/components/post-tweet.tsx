ca88070fa30c6928bc3c550afe489a76c6de0be5
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-11 16:24:23 -0400

    Add prop

--- src/app/components/post-tweet.tsx
+++ src/app/components/post-tweet.tsx
@@ -1,14 +1,19 @@
 import { Avatar } from '../../components/avatar'
+import { TweestType } from '../types/tweet.types'
 import { FormTweet } from './form-tweet'
 
-function PostTweet () {
+interface PostTweetProps {
+  updateTweets: (newTweet: TweestType) => void
+}
+
+function PostTweet ({ updateTweets }: PostTweetProps) {
   return (
     <div className='flex gap-5 mt-4'>
       <div>
         <Avatar src='https://i.pravatar.cc/150?u=a042581f4e29026024d' name='Jose Oviedo'/>
       </div>
 
-      <FormTweet />
+      <FormTweet updateTweets={updateTweets} />
     </div>
   )
 }
