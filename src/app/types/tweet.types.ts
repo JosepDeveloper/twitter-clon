724f26b33c8eedd00799149b8496d62584df90d6
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-25 15:28:02 -0400

    Update type

--- src/app/types/tweet.types.ts
+++ src/app/types/tweet.types.ts
@@ -1,4 +1,4 @@
-type urlImage = `https://i.pravatar.cc/150?u=a${string}`
+export type urlImage = `https://i.pravatar.cc/150?u=a${string}` | `https://avatars.githubusercontent.com/${string}`
 
 interface TweestType {
   textMarkdown: string
