43b6c55b42b0af85b95bda1690309909995ac5fd
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-25 10:25:36 -0400

    Add new button to header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { TwitterIcon } from '../../components/icons/twitter'
 import { HomeIcon } from '../../components/icons/home-icons'
 import { SearchIcons } from '@/components/icons/search-icons'
+import { ButtonLogin } from './button-login'
 
 interface HeaderProps {
   className?: string
@@ -9,47 +10,51 @@ interface HeaderProps {
 
 function Header ({ className }: HeaderProps) {
   return (
-    <header className={`${className} pt-5 flex flex-col gap-5 sticky top-0 h-fit`}>
-      <Link
-        href='/'
-        className={`
-          w-[70%] p-2 rounded-full
-          flex gap-2 justify-start items-center
-          hover:bg-gray-700/65 transition-colors ease-linear
-        `}
-      >
-        <TwitterIcon /> <span className='text-lg font-semibold'>Chigüire.Net</span>
-      </Link>
+    <header className={`${className} pt-5 flex flex-col sticky top-0 h-fit justify-between gap-10`}>
+      <div>
+        <Link
+          href='/'
+          className={`
+            w-[70%] p-2 rounded-full
+            flex gap-2 justify-start items-center
+            hover:bg-gray-700/65 transition-colors ease-linear
+          `}
+        >
+          <TwitterIcon /> <span className='text-lg font-semibold'>Chigüire.Net</span>
+        </Link>
 
-      <nav>
-        <ul className='list-none m-0 [&>li]:mt-0'>
-          <li>
-            <Link
-              href={'/'}
+        <nav>
+          <ul className='list-none m-0 [&>li]:mt-0'>
+            <li>
+              <Link
+                href={'/'}
+                className={`
+                  w-[50%] p-2 rounded-full
+                  flex gap-2 justify-start items-center
+                  hover:bg-gray-700/65 transition-colors ease-linear
+                `}
+              >
+                <HomeIcon /> <span className='font-medium'>Inicio</span>
+              </Link>
+
+            </li>
+            <li>
+              <Link
+              href={'#'}
               className={`
                 w-[50%] p-2 rounded-full
                 flex gap-2 justify-start items-center
                 hover:bg-gray-700/65 transition-colors ease-linear
               `}
-            >
-              <HomeIcon /> <span className='font-medium'>Inicio</span>
-            </Link>
+              >
+                <SearchIcons/> <span className='font-medium'>Explorar</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-          </li>
-          <li>
-            <Link
-            href={'#'}
-            className={`
-              w-[50%] p-2 rounded-full
-              flex gap-2 justify-start items-center
-              hover:bg-gray-700/65 transition-colors ease-linear
-            `}
-            >
-              <SearchIcons/> <span className='font-medium'>Explorar</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <ButtonLogin />
     </header>
   )
 }
