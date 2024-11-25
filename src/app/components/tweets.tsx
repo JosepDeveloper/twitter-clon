123e465b1782d930e4ffb6c4c9243de534152777
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-25 15:27:27 -0400

    Delete username

--- src/app/components/tweets.tsx
+++ src/app/components/tweets.tsx
@@ -5,12 +5,11 @@ import { Avatar } from '@/components/avatar'
 
 interface TweetsProps {
   textMarkdown: string
-  username: string
   nickname: string
   urlImage: string
 }
 
-function Tweets ({ nickname, username, textMarkdown, urlImage }: TweetsProps) {
+function Tweets ({ nickname, textMarkdown, urlImage }: TweetsProps) {
   return (
     <article className='p-2 flex gap-2'>
       <header>
@@ -20,7 +19,6 @@ function Tweets ({ nickname, username, textMarkdown, urlImage }: TweetsProps) {
       <div className='flex flex-col gap-2 w-full'>
         <div className='flex gap-2 items-center justify-start'>
           <h2 className='text-lg font-semibold tracki border-none scroll-m-none p-0 m-0'>{nickname}</h2>
-          <small className='text-sm font-extrabold text-white/30 m-0 p-0' style={{ margin: 0 }}>{username}</small>
         </div>
 
         <Markdown remarkPlugins={[reactGmf, remarkDirective]}>{textMarkdown}</Markdown>
