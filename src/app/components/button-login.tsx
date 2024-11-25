6d315d7543e09d461c057d699df64cf69cc54259
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-11-25 15:25:27 -0400

    Add function to singIn user

--- src/app/components/button-login.tsx
+++ src/app/components/button-login.tsx
@@ -1,8 +1,14 @@
+'use client'
 import { GithubIcon } from '@/components/icons/github-icon'
+import { signIn } from 'next-auth/react'
 
 function ButtonLogin () {
   return (
-    <button type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/70 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 me-2 mb-2 w-[226px] transition-colors ease-linear duration-75">
+    <button
+    type="button"
+    className="text-white bg-[#24292F] hover:bg-[#24292F]/70 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 me-2 mb-2 w-[226px] transition-colors ease-linear duration-75"
+    onClick={() => signIn('github')}
+    >
       <GithubIcon />
       Iniciar sesión con Github
     </button>
