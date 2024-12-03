f5a1c7ca034c1cc0c8578ca67fd7db5e134b0346
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-12-03 15:31:32 -0400

    Update control to datanase

--- frontend/src/app/hooks/use-tweets.ts
+++ frontend/src/app/hooks/use-tweets.ts
@@ -1,72 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TweestType } from '../types/tweet.types'
 
-const TWEEST_DEFAULT = [
-  {
-    id: 1,
-    textMarkdown: `# Esto es un ejemplo
-lorem ipsum dolor ammet sit amet, consectetur adipiscing elit. 
-    Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
-    Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
-    Proin porttitor, orci nec **nonummy molestie**, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
-    Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
-    Pellentesque hendrerit sapien. Donec enim diam, __elementum nec,__ pretium id, suscipit a, mi. 
-    Nam at lectus quam. Cras ornare. Pellentesque nec nisl sit amet sem tristique cursus. 
-    Proin euismod. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-    Curabitur et ligula.
-  
-*Ejemplo de Codigo*
-
-\`sudo apt-get install -y vim\`
-
-\`\`\`js
-
-const hello = 'Hello World!'
-console.log(hello)
-\`\`\`
-    `,
-    nickname: 'Jose Oviedo',
-    urlImage: 'https://i.pravatar.cc/150?u=a042581f4e29026024d'
-  },
-  {
-    id: 2,
-    textMarkdown: `# Esto es un ejemplo
-lorem ipsum dolor ammet sit amet, consectetur adipiscing elit. 
-    Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
-    Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
-    Proin porttitor, orci nec **nonummy molestie**, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
-    Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. 
-    Pellentesque hendrerit sapien. Donec enim diam, __elementum nec,__ pretium id, suscipit a, mi. 
-    Nam at lectus quam. Cras ornare. Pellentesque nec nisl sit amet sem tristique cursus. 
-    Proin euismod. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-    Curabitur et ligula.
-    
-- Lista 1
-- Lista 2
-- Lista 3
-    `,
-    nickname: 'Isaac Leon',
-    urlImage: 'https://i.pravatar.cc/150?u=a04258a2462d826712d'
-  },
-  {
-    id: 3,
-    textMarkdown: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur et ligula. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-    nickname: 'Javier Perez',
-    urlImage: 'https://i.pravatar.cc/150?u=a042581f4e29026704d'
-  }
-]
-
 function useTweets () {
-  const [tweets, setTweets] = useState(TWEEST_DEFAULT)
+  const [tweets, setTweets] = useState<TweestType[]>([])
 
-  const updateTweets = (newTweets: TweestType) => {
+  const updateTweets = async (newTweets: TweestType) => {
     const tweet = {
       ...newTweets,
       id: tweets.length + 1
     }
 
+    const response = await fetch('/api/tweets', {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: tweet.textMarkdown, username: tweet.nickname })
+    })
+    const data = await response.json()
+
+    console.log(data)
     setTweets(prevTwerst => [tweet, ...prevTwerst])
   }
+
+  useEffect(() => {
+    const getTweets = async () => {
+      const response = await fetch('/api/tweets', { cache: 'no-cache' })
+      const tweets = await response.json()
+
+      setTweets(tweets.data)
+    }
+
+    getTweets()
+  }, [])
 
   return {
     tweets,
