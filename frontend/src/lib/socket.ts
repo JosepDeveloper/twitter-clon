a905ca211c78bdef65da8a26ce4d9af259406a4f
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-12-03 15:53:11 -0400

    New changes

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL_SOCKET_SERVER = process.env.BACKEND_SOCKETS_URL || 'http://localhost:4000/'
+const URL_SOCKET_SERVER = 'https://twitter-clon-eight.vercel.app/'
 
 const socket = io(URL_SOCKET_SERVER)
 
