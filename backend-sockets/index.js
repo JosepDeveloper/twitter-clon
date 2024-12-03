440c7262e8c9efe65c5f17d9f1b4572e4271df17
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-12-03 16:11:22 -0400

    Add cors dependencies

--- backend-sockets/index.js
+++ backend-sockets/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import http from 'node:http'
 import logger from 'morgan'
+import cors from 'cors'
 import { Server as SocketServer } from 'socket.io'
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
