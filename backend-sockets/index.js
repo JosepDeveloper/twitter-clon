06fb674665ee37084cbb0c00264b70e44f2c357f
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-12-03 16:16:56 -0400

    Delete origin

--- backend-sockets/index.js
+++ backend-sockets/index.js
@@ -9,11 +9,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new SocketServer(server, {
-  cors: {
-    origin: [],
-  },
-});
+const io = new SocketServer(server);
 
 app.use(logger('dev'));
 
